9e1ce41a06a5d6026c542b51aa1d7c06ca28fe14
GodsDogSled <gabekelly@hotmail.ca>
2023-09-09 16:01:53 -0700

    added priority prop to above the fold header images

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -6,8 +6,8 @@ import banner from ".././app/images/banner.jpg"
 export default function PageHeader({ title }) {
   return (
     <div className="page-banner">
-      <Image className="mobile-show" style={{ objectFit: "cover" }} width={1185} height={200} sizes="100vw" src={banner} alt={"clean kitchen"} />
-      <Image className="mobile-hide" style={{ objectFit: "cover" }} layout="responsive" sizes="100vw" src={banner} alt={"clean kitchen"} />
+      <Image priority className="mobile-show" style={{ objectFit: "cover" }} width={1185} height={200} sizes="100vw" src={banner} alt={"clean kitchen"} />
+      <Image priority className="mobile-hide" style={{ objectFit: "cover" }} layout="responsive" sizes="100vw" src={banner} alt={"clean kitchen"} />
       <div className="gradient">
 
       </div>
