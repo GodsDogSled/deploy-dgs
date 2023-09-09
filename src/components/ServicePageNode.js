43ab19a5de7eb1640c0a0fe1fa1bf832b004f9f5
GodsDogSled <gabekelly@hotmail.ca>
2023-09-09 12:24:31 -0700

    service image priority

--- src/components/ServicePageNode.js
+++ src/components/ServicePageNode.js
@@ -8,7 +8,7 @@ export default function ServicePageNode({ room, photo, list }) {
   return (
     <section className="service-page-node">
       <div className="image-container">
-        <Image width={100} height={200} sizes="50vw" className="pic" style={{ objectFit: "cover", borderRadius: "0px 15px 15px 15px", border: "solid 6px white" }} layout="responsive" src={photo} alt={`image of ${room}`}></Image>
+        <Image priority width={100} height={200} sizes="50vw" className="pic" style={{ objectFit: "cover", borderRadius: "0px 15px 15px 15px", border: "solid 6px white" }} layout="responsive" src={photo} alt={`image of ${room}`}></Image>
         <Link className="hover-link" href={"/quote"} ><span className="quote-button2">Get a Quote</span></Link>
       </div>
       <div className="text-container">
