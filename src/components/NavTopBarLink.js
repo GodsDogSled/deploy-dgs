4facc2bd116936a3ad9eb4e079edbffd2f3b6b6e
GodsDogSled <gabekelly@hotmail.ca>
2023-11-06 11:56:56 -0800

    attempt 2 adding messanger and whatsapp icon links

--- src/components/NavTopBarLink.js
+++ src/components/NavTopBarLink.js
@@ -1,10 +1,15 @@
 import Image from "next/image"
+import React from "react";
 
 export default function NavTopBarLink({ img, header, info, handleLink }) {
+
+  const isValid = React.isValidElement(img);
+
+
   return (
     <li className="nav-li">
-      <a aria-label="email link" href={handleLink} style={{ display: "flex" }} className={handleLink ? "email-link" : ""}>
-        <Image className="cs-icon mobile-hide" src={img} alt="icon" loading="lazy" decoding="async" width="24" height="24" aria-hidden="true" />
+      <a target="blank" aria-label="email link" href={handleLink} style={{ display: "flex" }} className={handleLink ? "email-link" : ""}>
+        {isValid ? img : <Image className="cs-icon mobile-hide" src={img} alt="icon" loading="lazy" decoding="async" width="24" height="24" aria-hidden="true" />}
         <div className="text-content mobile-hide">
           <span className="mobile-hide">{header}</span>
           <span id="info">{info}</span>
