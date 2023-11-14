661784fd682b4d75c91e211739e7250562d6992f
GodsDogSled <gabekelly@hotmail.ca>
2023-11-14 11:43:05 -0800

    removed uncrawlable links

--- src/components/NavTopBarLink.js
+++ src/components/NavTopBarLink.js
@@ -8,13 +8,25 @@ export default function NavTopBarLink({ img, header, info, handleLink }) {
 
   return (
     <li className="nav-li">
-      <a target="blank" aria-label={handleLink ? `${header} link` : ""} href={handleLink} style={{ display: "flex" }} className={handleLink ? "email-link" : ""}>
+      {handleLink ? <a target="blank" aria-label={`${header} link`} href={handleLink} style={{ display: "flex" }} className="email-link">
         {isValid ? img : <Image className="cs-icon mobile-hide" src={img} alt="icon" loading="lazy" decoding="async" width="24" height="24" aria-hidden="true" />}
         <div className="text-content mobile-hide">
           <span className="mobile-hide">{header}</span>
           <span id="info">{info}</span>
         </div>
+        {/* <div className="text-content mobile-hide">
+          <span className="mobile-hide">{header}</span>
+          <span id="info">{info}</span>
+        </div> */}
       </a>
+        :
+        <div style={{ display: "flex" }}>
+          {isValid ? img : <Image className="cs-icon mobile-hide" src={img} alt="icon" loading="lazy" decoding="async" width="24" height="24" aria-hidden="true" />}
+          <div className="text-content mobile-hide">
+            <span className="mobile-hide">{header}</span>
+            <span id="info">{info}</span>
+          </div>
+        </div>}
     </li>
   )
 }
